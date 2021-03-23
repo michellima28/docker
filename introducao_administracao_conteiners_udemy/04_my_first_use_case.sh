8c6f3447a0deb4ed4c6264d1492a98d4386a133a
Michel Lima <michel@pop-os.localdomain>
2021-03-23 20:15:15 -0300

    new hands on - compiling c code

--- introducao_administracao_conteiners_udemy/04_my_first_use_case.sh
+++
@@ -1,7 +0,0 @@
-# MODE 1: scan an IP address using nmap tool through a nmap image
-sudo docker run --rm uzyexe/nmap -sS google.com
-
-# MODE 2: scan an IP address using nmap tool through an ubuntu image
-sudo docker run -it --name nmap ubuntu:18.04 bash
-
-apt-get update && apt-get install nmap && nmap -sS google.com
